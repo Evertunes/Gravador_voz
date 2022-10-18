4678c7610ee05cf5865fadc57be4a892cffe4ab1
Everton Alves <alveseverton023@gmail.com>
2022-10-18 06:02:26 -0300

    mensagens

--- gravador.py
+++ gravador.py
@@ -1,9 +1,10 @@
 import sounddevice
 from scipy.io.wavfile import write
-sr = 44100
-seconds = 5
+sr = 44100 #frequencia do audio
+seconds = 5 #tempo de gravação (5 segundos)
 print('Recording...\n')
 record_voice = sounddevice.rec(sr*seconds, samplerate = sr, channels = 2)
 sounddevice.wait()
+print('Salvando...\n')
 write('mynewfile.wav', sr, record_voice)
-print('Finished')
+print('Finished!')
